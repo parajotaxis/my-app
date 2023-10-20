8f887f6c3dc7ef8353b70b124932fe8a3ff22a66
Jefferson Xavier <parajotaxis@gmail.com>
2023-10-19 21:28:10 -0300

    add var nome para comp filho

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   
   constructor() { }
-  @Input() nome:string = 'Jefferson Xavier';
+  @Input() nome:string = '';
   ngOnInit(): void {
     
   }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  
+  constructor() { }
+  nome:string = "Jefferson Xavier do N";
+  ngOnInit(): void {
+    
+  }
 
 }
