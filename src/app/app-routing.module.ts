222bd07c5a9fcece73bf978b6d230e76b4c59dd6
Jefferson Xavier <parajotaxis@gmail.com>
2023-11-07 17:22:22 -0300

    add db.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListarComponent } from './pages/listar/listar.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ManipulandoJsonComponent } from './pages/manipulando-json/manipulando-json.component';
 
 const routes: Routes = [
   {path:'', redirectTo: '/home', pathMatch: 'full'},
   {path:'home', component: HomeComponent},
   {path:'listar', component: ListarComponent},
   {path:'cadastro', component: CadastroComponent},
-  {path:'login', component: LoginComponent}
+  {path:'login', component: LoginComponent},
+  {path:'json', component: ManipulandoJsonComponent}
 ];
 
 @NgModule({
---
+++ src/app/models/user.ts
@@ -0,0 +1,6 @@
+export class User{
+    publlic id: string = '';
+    publlic firstName: string = '';
+    publlic phone: string = '';
+    publlic email: string = '';
+}
